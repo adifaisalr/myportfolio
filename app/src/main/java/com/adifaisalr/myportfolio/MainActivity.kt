3477051df59f2ab864d969933302d3c2e505144a
Adi Faisal Rahman <adifaisal.r@gmail.com>
2025-04-19 22:35:27 +0700

    simulating state change in EducationScreen

--- app/src/main/java/com/adifaisalr/myportfolio/MainActivity.kt
+++ app/src/main/java/com/adifaisalr/myportfolio/MainActivity.kt
@@ -6,11 +6,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -109,23 +113,47 @@ fun EducationScreen(modifier: Modifier = Modifier, viewModel: EducationViewModel
     val uiState by viewModel.uiState.collectAsState()
 
     Box(modifier) {
-        when (uiState) {
-            EducationViewModel.UiState.Empty -> {
-                Text(
-                    text = "Education Is Empty",
-                    modifier = Modifier.padding(10.dp)
-                )
-            }
-
-            is EducationViewModel.UiState.Loaded -> {
-                Text(
-                    text = "Loaded",
-                    modifier = Modifier.padding(10.dp)
-                )
-            }
-
-            EducationViewModel.UiState.Loading -> {
+        when {
+            uiState.isLoading -> {
                 CircularProgressIndicator()
+            }
+
+            uiState.isError -> {
+                Text(
+                    text = "Error",
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+
+            uiState.educationList.isEmpty() -> {
+                Row {
+                    Text(
+                        text = "Education Is Empty",
+                        modifier = Modifier.padding(10.dp)
+                    )
+                    Button(onClick = {
+                        viewModel.updateState(
+                            EducationViewModel.UiState(
+                                educationList = listOf("University","High School"),
+                                isError = false,
+                                isLoading = false
+                            )
+                        )
+                    }) {
+                        Text(text = "Refresh")
+                    }
+                }
+            }
+
+            else -> {
+                LazyColumn{
+                    items(uiState.educationList){
+                        Text(
+                            text = it,
+                            modifier = Modifier.padding(10.dp)
+                        )
+                    }
+                }
             }
         }
     }
